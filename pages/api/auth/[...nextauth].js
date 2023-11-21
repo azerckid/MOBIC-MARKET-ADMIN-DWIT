e63105b6a21605e3d7310d71e777febec9e872c0
남형석 <azerc@MacMini.local>
2023-11-21 17:54:55 +0900

    add NEXTAUTY_SECRET

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 const adminEmails = ["azerckid@gmail.com"];
 export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
